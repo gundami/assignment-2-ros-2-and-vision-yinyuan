b62868a3e5ee3faab623aacf6674f24fc921dfd4
root <gundami0096@outlook.com>
2025-11-26 02:27:59 +0000

    Finish

--- aisd_vision/aisd_vision/image_publisher.py
+++ aisd_vision/aisd_vision/image_publisher.py
@@ -9,8 +9,8 @@ import cv2
 class ImagePublisher(Node):
     def __init__(self):
         super().__init__('image_publisher')
-        self.publisher_ = self.create_publisher(Image, 'video_frames', 10)
-        timer_period = 0.1  # seconds
+        self.publisher_ = self.create_publisher(Image, 'camera/image_raw', 10)         
+        timer_period = 0.1
         self.timer = self.create_timer(timer_period, self.timer_callback)
         
         self.cap = cv2.VideoCapture(0)
@@ -20,7 +20,7 @@ class ImagePublisher(Node):
         ret, frame = self.cap.read()
         if ret == True:
             self.publisher_.publish(self.br.cv2_to_imgmsg(frame))
-            self.get_logger().info('Publishing video frame')
+            #self.get_logger().info('Publishing video frame')
 
 def main(args=None):
     rclpy.init(args=args)
